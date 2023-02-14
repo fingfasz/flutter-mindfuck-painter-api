23069222dccb656af850f339ac6b1ff451132b72
rokuwu <rokuwu@gmail.com>
2023-02-14 15:21:34 +0100

    sketches

--- src/api/controllers/sketches_controller.js
+++ src/api/controllers/sketches_controller.js
@@ -4,14 +4,46 @@ const { Op } = require('sequelize');
 const { checkToken } = require('../utils/functions');
 
 const createSketch = async (req, res) => {
-    const token = checkToken(req, res);
+    try {
+        const token = checkToken(req, res);
 
-    const { uuid } = token.value;
-    const { receiverUUID, bin } = req.body;
-
-    const sketch = await Sketch.create({ sender_uuid: uuid, receiver_uuid: receiverUUID, bin });
-
-    res.status(201).send({ sketch });
+        const { uuid } = token.value;
+        const { receiverUUID, bin } = req.body;
+    
+        const sketch = await Sketch.create({ sender_uuid: uuid, receiver_uuid: receiverUUID, bin });
+    
+        res.status(201).send({ sketch });
+    } catch (err) {
+        res.status(400).send(err);
+    }
 }
 
-module.exports = { createSketch }
+const deleteSketch = async (req, res) => {
+    try {
+        const { uuid } = req.body;
+
+        const sketch = await Sketch.findOne({
+            where: {
+                uuid 
+            }
+        });
+
+        if (!sketch){
+            res.status(400).send({ message: 'invalid sketch' })
+        } else {
+            await Sketch.destroy({
+                where: {
+                    uuid 
+                }
+            });
+
+            res.status(200).send();
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err);
+    }
+}
+
+module.exports = { createSketch, deleteSketch }
--- src/api/routes/sketches_router.js
+++ src/api/routes/sketches_router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createSketch } = require('../controllers/sketches_controller');
+const { createSketch, deleteSketch } = require('../controllers/sketches_controller');
 
 router.post('/create', createSketch);
+router.post('/delete', deleteSketch);
 
 module.exports = router;
